1c4f0d828b0bea75150b71e15528170646c33e42
Guillem B <guillembaracco@gmail.com>
2022-07-29 08:27:25 +0200

    iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
 import './App.css';
-import productData from './products.json';
+import React, { useState } from 'react';
+import taskData from './tasks.json';
+import TaskCard from './components/TaskCard';
+import SearchBar from './components/SearchBar';
 
 function App() {
-  const [products, setProducts] = useState(productData);
+  const [pendingTasks, setPendingTasks] = useState(taskData);
+  const [filteredTasks, setFilteredTasks] = useState(taskData);
+
+  const handleDelete = (id) => {
+    const updatedList = pendingTasks.filter(elem => elem._id !== id);
+    setPendingTasks(updatedList);
+    setFilteredTasks(updatedList);
+  }
+
+  const handleSearch = (searchValue) => {
+    if (searchValue === '') {
+      setFilteredTasks(pendingTasks)
+    } else {
+      const filtered = pendingTasks.filter(elem => elem.title.toLowerCase().includes((searchValue).toLowerCase()));
+      setFilteredTasks(filtered)
+    }
+  }
 
   return (
-    <div className="cart">
-      <h1>My shopping cart</h1>
-
-      {/* Your code goes here */}
-
+    <div className="App">
+      <h1>To-do list</h1>
+      <SearchBar onSearch={handleSearch} />
+      {filteredTasks.map(elem => {
+        return <TaskCard key={elem._id} task={elem} onDelete={handleDelete} />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
